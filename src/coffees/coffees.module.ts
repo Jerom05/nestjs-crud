856d09e64ad9b043c5dd746d157b1c32534d781f
jerom ghagra <jeromghagra@gmail.com>
2025-04-15 15:28:43 +0600

    feat: add environment-specific configuration services to CoffeesModule

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,6 +7,10 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity/event.entity';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
@@ -15,6 +19,13 @@ import { Event } from '../events/entities/event.entity/event.entity';
     {
       provide: 'COFFEE_BRANDS',
       useValue: ['Buddy Brew', 'Blue Bottle', 'Drip'],
+    },
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
     },
   ],
   exports: [CoffeesService],

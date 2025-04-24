034d56c14eacb52ca671fcca55784c58fce1879c
jerom ghagra <jeromghagra@gmail.com>
2025-04-24 14:22:37 +0600

    refactor: move TypeOrmModule configuration to use async factory for better flexibility

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,24 +10,26 @@ import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        logging: true,
+        autoLoadEntities: true,
+      }),
+    }),
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         DATABASE_HOST: Joi.string().required(),
         DATABASE_PORT: Joi.string().required().default(5432),
         DATABASE_USER: Joi.string().required(),
       }),
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      logging: true,
-      autoLoadEntities: true,
     }),
     CoffeesModule,
     CoffeeRatingModule,

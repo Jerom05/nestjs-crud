601ce95e0089c8a5ff6bc5dfec46c3ed58d172ae
jerom ghagra <jeromghagra@gmail.com>
2025-05-02 15:55:14 +0600

    feat: add WrapResponseInterceptor for response wrapping in global scope

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { HttpExecptionFilter } from './common/filters/http-execption/http-execption.filter';
 import { ApiKeyGuard } from './common/guards/api-key/api-key.guard';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExecptionFilter());
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

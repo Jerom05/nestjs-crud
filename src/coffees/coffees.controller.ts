00dd97ec3b0a15e6cb16e6673697dfd10b6901e8
jerom ghagra <jeromghagra@gmail.com>
2025-05-11 15:34:52 +0600

    feat: enhance Coffee API with Swagger documentation and update DTO imports

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -15,12 +15,15 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@Public()
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @Public()
   @Get()
   findAll(@Query() query: PaginationQueryDto) {
     return this.coffeesService.findAll(query);

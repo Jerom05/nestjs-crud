7bf7813dc0880057db0c3abaa26b041a3794127a
jerom ghagra <jeromghagra@gmail.com>
2025-05-11 08:46:26 +0600

    feat: integrate Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpExecptionFilter } from './common/filters/http-execption/http-execpt
 import { ApiKeyGuard } from './common/guards/api-key/api-key.guard';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptor/timeout/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,6 +24,14 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new TimeoutInterceptor(),
   );
+  const options = new DocumentBuilder()
+    .setTitle('Nest crud')
+    .setDescription('CRUD API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

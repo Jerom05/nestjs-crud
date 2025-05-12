555a6169effb3643175d01bd58faea7c26ff81f8
jerom ghagra <jeromghagra@gmail.com>
2025-05-12 15:34:07 +0600

    test: enhance CoffeesService tests with mock repository implementations for Flavor and Coffee

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesService } from './coffees.service';
-import { Connection } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
+import { ObjectLiteral } from 'typeorm';
+
+type MockRepository<T extends ObjectLiteral = any> = Partial<
+  Record<keyof Repository<T>, jest.Mock>
+>;
+const createMockRepository = <
+  T extends ObjectLiteral = any,
+>(): MockRepository<T> => ({
+  findOne: jest.fn(() => undefined), // Ensure findOne is always defined
+  create: jest.fn(),
+});
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
+  let coffeeRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,19 +34,32 @@ describe('CoffeesService', () => {
         },
         {
           provide: getRepositoryToken(Flavor),
-          useValue: {},
+          useValue: createMockRepository(),
         },
         {
           provide: getRepositoryToken(Coffee),
-          useValue: {},
+          useValue: createMockRepository(),
         },
       ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
+    coffeeRepository = module.get(getRepositoryToken(Coffee));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    describe('when coffee with ID exists', () => {
+      it('should return the coffee object', async () => {
+        const coffeeId = 1;
+        const expectedCoffee = {};
+        coffeeRepository?.findOne?.mockReturnValue(expectedCoffee);
+        const coffee = await service.findOne(1);
+        expect(coffee).toBeDefined();
+      });
+    });
   });
 });
